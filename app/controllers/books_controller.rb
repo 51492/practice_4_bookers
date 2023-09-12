14febe36ac4dbcadde431ecb52c754159d6c7a83
TeppeiYamashita <143589587+51492@users.noreply.github.com>
2023-09-12 13:11:43 +0000

    [update]assignment2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,22 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-    @books = Book.all
-  end
+  # def new
+  #   @book = Book.new
+  #   @books = Book.all
+  # end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id) #投稿詳細画面へ
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice]= "Book was successfully created."
+      redirect_to book_path(@book.id) #投稿詳細画面へ
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
@@ -21,11 +27,22 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
   end
 
 
