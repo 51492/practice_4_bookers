2ebee352c589cc0b014cdcfb3ca5644298ca406d
TeppeiYamashita <143589587+51492@users.noreply.github.com>
2023-09-12 08:15:31 +0000

    [update]assignment2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,21 @@
 class BooksController < ApplicationController
   def new
     @book = Book.new
+    @books = Book.all
   end
 
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to "/top"
+    redirect_to book_path(book.id) #投稿詳細
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
